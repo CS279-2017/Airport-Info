d9617faf0692e96e7b79d767492cfb68841129c5
Blake Arledge <richard.b.arledge@vanderbilt.edu>
2017-02-28 16:51:16 -0600

    attempted implementation of navigator

--- TRVLR/index.android.js
+++ TRVLR/index.android.js
@@ -1,53 +1,37 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, Navigator } from 'react-native';
 
-export default class TRVLR extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+import firsttimeuser from './AndroidComponents/firsttimeuser';
+
+class TRVLR extends Component {
+    render() {
+        return (
+            <Navigator
+                initialRoute={{ title: 'My Initial Scene', index: 0 }}
+                renderScene={(route, navigator) =>
+                    <firsttimeuser
+                        title={route.title}
+
+                        // Function to call when a new scene should be displayed
+                        onForward={() => {
+                            const nextIndex = route.index + 1;
+                            navigator.push({
+                                title: 'Scene ' + nextIndex,
+                                index: nextIndex,
+                            });
+                        }}
+
+                        // Function to call to go back to the previous scene
+                        onBack={() => {
+                            if (route.index > 0) {
+                                navigator.pop();
+                            }
+                        }}
+                    />
+                }
+            />
+        )
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('TRVLR', () => TRVLR);
