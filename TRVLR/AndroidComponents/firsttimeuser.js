2cf2ca3be0b5a2aebba761b0a2411964c70c8a43
Blake Arledge <richard.b.arledge@vanderbilt.edu>
2017-02-28 23:25:27 -0600

    restyled splash screen

--- TRVLR/AndroidComponents/firsttimeuser.js
+++ TRVLR/AndroidComponents/firsttimeuser.js
@@ -6,97 +6,113 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  Button,
-  Navigator,
-  Alert
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    Button,
+    Navigator,
+    Alert
 } from 'react-native';
 
-import profile1 from './profile1.js';
 
 const onButtonPress = () => {
-    this.props.navigator.push({
-        title: "profile1",
-        component: profile1
-    });
+    Alert.alert('Button has been pressed!');
 }
 
-
 export default class firsttimeuser extends Component {
-  render() {
-    return (
-		<View style={styles.container}>
-			<Image style={styles.backgroundImage}
-				source={require('./welcomebackground.png')}>
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image style={styles.backgroundImage}
+                       source={require('./Unknown.png')}>
 
-				<Text style={styles.welcome}>
-					Welcome to TL;VR!
-				</Text>
-
-				<Text style={styles.instructions}>
-					To start your customized experience, click "get started" below
-				</Text>
-
-			</Image>
+                    <Text style={styles.welcome}>
+                        Welcome to <Text style={styles.appName}>
+                        TRVLR
+                    </Text>
+                    </Text>
 
 
-			<View style={styles.container2}>
-				<View style={styles.box3}>
-					<Button
-						onPress={onButtonPress}
-						title="Get started"
-						color="#42f44e"
-						accessibilityLabel="Learn more about this purple button"
-						alignSelf="stretch"
-					/>
-				</View>
-			</View>
-		</View>
-    );
-  }
 
 
+                    <Text style={styles.instructions}>
+                        To start your customized experience, click "Get Started" below
+                    </Text>
+
+                </Image>
+
+
+                <View style={styles.container2}>
+                    <View style={styles.box3}>
+                        <Button
+                            onPress={onButtonPress}
+                            title="Get Started"
+                            color="cornflowerblue"
+                            accessibilityLabel="Learn more about this purple button"
+                            alignSelf="stretch"
+                        />
+                    </View>
+                </View>
+            </View>
+        );
+    }
 }
 
 
 
 
 const styles = StyleSheet.create({
-   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  backgroundImage: {
-    flex: 1,
-    resizeMode: 'cover', // or 'stretch'
-  },
-  welcome: {
-    fontSize: 34,
-    textAlign: 'center',
-    margin: 10,
-	color: '#207c76',
-	fontWeight: 'bold'
-  },
-  instructions: {
-	fontSize: 16,
-    	textAlign: 'left',
-    	margin: 10,
-	marginTop: 90,
-	color: '#207c76'
-  },
-  container: {
-    flex: .5,
-    flexDirection: 'column',
-    justifyContent: 'center', //replace with flex-end or center
-    borderBottomWidth: 1,
-    borderBottomColor: '#000'
-  }
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'transparent'
+    },
+    backgroundImage: {
+
+        width: 400,
+        height: 620
+    },
+    welcome: {
+        fontFamily: "Hiragino Mincho ProN",
+        fontSize: 34,
+        textAlign: 'center',
+        margin: 20,
+
+        color: 'black',
+
+
+
+        backgroundColor: 'transparent'
+    },
+    appName: {
+        fontFamily: "Futura",
+        fontSize: 34,
+
+        backgroundColor: 'transparent'
+
+    },
+    instructions: {
+
+        fontSize: 15,
+        fontFamily: "Hiragino Mincho ProN",
+        textAlign: 'center',
+        fontWeight: 'bold',
+        paddingRight: 15,
+
+        marginTop: 150,
+        color: 'black',
+        backgroundColor: 'transparent'
+    },
+    container: {
+        flex: .5,
+        flexDirection: 'column',
+        justifyContent: 'center', //replace with flex-end or center
+        borderBottomWidth: 1,
+        borderBottomColor: 'transparent'
+    }
 });
 
-AppRegistry.registerComponent('FirstTimeUser', () => firsttimeuser);
+AppRegistry.registerComponent('firsttimeuser', () => firsttimeuser);
