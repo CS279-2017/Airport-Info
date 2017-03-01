46417bea3f21cacdd3961ec49f70e15de0f31341
Blake Arledge <richard.b.arledge@vanderbilt.edu>
2017-02-28 19:45:55 -0600

    first sucessful navigator use

--- TRVLR/index.android.js
+++ TRVLR/index.android.js
@@ -1,37 +1,43 @@
 import React, { Component } from 'react';
-import { AppRegistry, Navigator } from 'react-native';
+import { AppRegistry, Navigator, StyleSheet } from 'react-native';
 
 import firsttimeuser from './AndroidComponents/firsttimeuser';
 
+
 class TRVLR extends Component {
-    render() {
+
+    renderScene(route, navigator) {
+        return <route.component {...route.passProps} navigator={navigator}/>
+    }
+
+    configureScene (route, routeStack){
+        if(route.type === 'Modal'){
+            return Navigator.SceneConfigs.FloatFromBottom
+        }
+        return Navigator.SceneConfigs.PushFromRight
+    }
+
+    render(){
         return (
             <Navigator
-                initialRoute={{ title: 'My Initial Scene', index: 0 }}
-                renderScene={(route, navigator) =>
-                    <firsttimeuser
-                        title={route.title}
+                configureScene={this.configureScene.bind(this)}
+                renderScene={this.renderScene.bind(this)}
 
-                        // Function to call when a new scene should be displayed
-                        onForward={() => {
-                            const nextIndex = route.index + 1;
-                            navigator.push({
-                                title: 'Scene ' + nextIndex,
-                                index: nextIndex,
-                            });
-                        }}
+                initialRoute={{
+                    component: firsttimeuser,
+                }}/>
 
-                        // Function to call to go back to the previous scene
-                        onBack={() => {
-                            if (route.index > 0) {
-                                navigator.pop();
-                            }
-                        }}
-                    />
-                }
-            />
-        )
+        );
     }
 }
+
+const styles = StyleSheet.create({
+    navigationContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
+    }
+});
 
 AppRegistry.registerComponent('TRVLR', () => TRVLR);
